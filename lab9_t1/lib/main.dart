2b47cc87be447b2db178c95f21f05a5bc7b6307c
jaysohagiya54 <85229854+jaysohagiya54@users.noreply.github.com>
2022-09-18 17:30:43 +0530

    Add files via upload

---
+++ lab9_t1/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:lab9_t1/quote.dart'; // Absolute path
+//import 'quote.dart'; // due to same directory file relative path will work
+import 'quote_card.dart'; // Relative path
+
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+/*
+List<String> quotes = [
+'The truth is realy pure and never simple',
+'I see humans but no humanity',
+'The time is always right to do what is right'
+];
+// List<String> author = [];
+*/
+/* this will create error..because list is now not of string....
+List<String> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'jignesh1'),
+];
+*/
+  List<Quote> quotes = [
+    Quote(text: 'The purpose of our lives is to be happy.',author: 'Dalai Lama'),
+    Quote(author: 'John Lennon', text: 'Get busy living or get busy dying.'),
+    Quote(text: 'In order to write about life first you must live it',author: 'Ernest Hemingway'),
+  ];
+/*
+Widget quoteTemplate(quote){
+return QuoteCard(quote: quote,);
+}
+*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal[100],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          },
+        )).toList(),
+      ),
+    );
+  }
+}
